e3d2849f281263f679504dd4f7f5a4599eef4694
이용진 <yongjin_lee@daekyo.co.kr>
2021-10-05 17:01:35 +0900

    d

--- Test Suites/WEB2/TCS_WEB2.ts
+++ Test Suites/WEB2/TCS_WEB2.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>TSC_WEB2</name>
+   <name>TCS_WEB2</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
